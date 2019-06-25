90d646ecf1a1ee7ce70303a30729f05cacdc3f0d
shankarvarat <shankarvarat@gmail.com>
2019-06-25 10:20:24 +0530

    new updated models

--- Todo_project/Todo_app/models.py
+++ Todo_project/Todo_app/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 # Create your models here.
+H="High"
+L="Low"
+A="Avarage"
+c="Completed"
+w="Work in Progress"
+n="Not Started"
+crik=[(H,"High"),(L,"Low"),(A,"Avarage")]
+stat=[(c,"Completed"),(w,"Work in Progress"),(n,"Not Started")]
 class todo(models.Model):
-    work=models.TextField(max_length=300)
-    created_at=models.DateField()
-    completed=models.BooleanField(default=False)
+    title=models.CharField(max_length=100)
+    description=models.TextField(max_length=300)
+    created_date=models.DateField(auto_now_add=True)
+    due_date=models.DateField()
+    criticality=models.CharField(max_length=100,choices=crik)
+    status=models.CharField(max_length=100,choices=stat)
+
+
     def __str__(self):
-        return self.work
+        return self.title
